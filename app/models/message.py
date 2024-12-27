dbc0e91370bae906e2edffc348c39da0edf370a8
Nazlı As <f.nazlias@gmail.com>
2024-12-27 17:12:38 +0300

    Change timestamp of Message Model

--- app/models/message.py
+++ app/models/message.py
@@ -1,15 +1,13 @@
 from datetime import datetime
-
-from app.utils.db import Base  # Import the Base class
+from sqlalchemy import func
+from app.utils.db import Base  
 from sqlalchemy.orm import Mapped, mapped_column
-from app.utils.db import Base
+
 
 class Message(Base):
     __tablename__ = "messages"
 
-     
-    id :Mapped[int]= mapped_column(primary_key=True, index=True)
-    is_ai: Mapped[bool]= mapped_column(nullable=False)
-    content : Mapped[str]= mapped_column( nullable=False)
-    timestamp :Mapped[datetime]= mapped_column(nullable=False)
-
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    is_ai: Mapped[bool] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
+    timestamp: Mapped[datetime] = mapped_column(default=func.now(), nullable=False)
