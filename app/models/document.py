16a2cb22f6a55474005abe77ff6c4e1ca015258c
Nazlı As <f.nazlias@gmail.com>
2024-12-27 17:09:42 +0300

    Add unique feature to title and path of Document Model

--- app/models/document.py
+++ app/models/document.py
@@ -6,5 +6,5 @@ class Document(Base):
     __tablename__ = "documents"
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     is_processed: Mapped[bool] = mapped_column(nullable=False)
-    title: Mapped[str] = mapped_column(nullable=False)
-    file_path: Mapped[str] = mapped_column(nullable=False)
+    title: Mapped[str] = mapped_column(nullable=False, unique=True)
+    file_path: Mapped[str] = mapped_column(nullable=False, unique=True)
